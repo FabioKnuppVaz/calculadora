e35dd3555b805e811ae39c7f9521c11b98a0fdd4
fabio <fabio.knupp@gmail.com>
2023-01-26 16:35:40 -0300

    correcao construtor

--- src/main/java/app/model/CalculadoraModel.java
+++ src/main/java/app/model/CalculadoraModel.java
@@ -5,14 +5,18 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CalculadoraModel {
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Integer id;
